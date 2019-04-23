c021dc685e69f22849469a5f8b01d9e88e3428a2
jeisontecnico <jeisontecnico@yahoo.com.br>
2019-04-23 12:07:55 -0300

    Add Slider

--- app/src/main/java/com/jepsolucoes/organizzeclone/view/MainActivity.java
+++ app/src/main/java/com/jepsolucoes/organizzeclone/view/MainActivity.java
@@ -3,13 +3,38 @@ package com.jepsolucoes.organizzeclone.view;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 import com.jepsolucoes.organizzeclone.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.holo_orange_dark)
+                .fragment(R.layout.intro_1)
+                .build()
+        );
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.holo_blue_dark)
+                .fragment(R.layout.intro_2)
+                .build()
+        );
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.holo_green_dark)
+                .fragment(R.layout.intro_3)
+                .build()
+        );
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.holo_red_dark)
+                .fragment(R.layout.intro_4)
+                .build()
+        );
+
+
     }
 }
